af3b7fc1e3822a3ec12e4d9f7fcd2bb3d31c3a97
Eden Biran <edenbiran@mail.tau.ac.il>
2023-04-09 23:09:06 +0300

    Revert "TestCase now includes Fact being edited"
    This reverts commit 9954312e6547c870cf2e81593a49b783969b2452.

--- query.py
+++ query.py
@@ -8,22 +8,9 @@ class Query:
         self._relation = relation
         self._targets_ids = target_ids if type(target_ids) == list else [target_ids]
 
-    def get_query_prompt(self):
-        return self._relation.phrase(get_label(self._subject_id))
-
-    def get_answers(self):
-        answers = []
-        for target in self._targets_ids:
-            if type(target) is str:
-                target_answer = [get_label(target)] + get_aliases(target)
-            else:
-                target_answer = [str(target)]
-            answers.append(target_answer)
-        return answers
-
     def to_dict(self):
         return {
-            'input_prompt': self.get_query_prompt(),
+            'input_prompt': self._relation.phrase(get_label(self._subject_id)),
             'answers': [{'value': get_label(target), 'aliases': get_aliases(target)} if type(target) == str
                         else {'value': str(target), 'aliases': []} for target in self._targets_ids]
         }
