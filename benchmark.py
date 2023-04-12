7df460cdbb75038ff8696e749abc1532e3c5e061
roicohen93 <roi3993@gmail.com>
2023-04-12 14:47:42 +0200

    updates

--- benchmark.py
+++ benchmark.py
@@ -19,6 +19,46 @@ class Example:
         self.prev_storage_tests = prev_storage_tests
 
 
+class CounterFactualExample(Example):
+
+    def __init__(self,
+                 fact: Fact,
+                 previous_fact: Fact,
+                 making_up_tests: list,
+                 logical_constraints: list,
+                 subject_paraphrasing_tests: list,
+                 two_hop_tests: list,
+                 prev_storage_tests: list):
+        super().__init__(
+            fact,
+            making_up_tests,
+            logical_constraints,
+            subject_paraphrasing_tests,
+            two_hop_tests,
+            prev_storage_tests
+        )
+        self.previous_fact = previous_fact
+
+
+class RecentlyAddedExample(Example):
+
+    def __init__(self,
+                 fact: Fact,
+                 making_up_tests: list,
+                 logical_constraints: list,
+                 subject_paraphrasing_tests: list,
+                 two_hop_tests: list,
+                 prev_storage_tests: list):
+        super().__init__(
+            fact,
+            making_up_tests,
+            logical_constraints,
+            subject_paraphrasing_tests,
+            two_hop_tests,
+            prev_storage_tests
+        )
+
+
 class Dataset:
 
     def __init__(self, examples: list):
