e7688c96816abec6d886ec7030b98c4129bc2f56
Eden Biran <edenbiran@mail.tau.ac.il>
2023-04-16 13:34:07 +0300

    Add MEND model editor

--- modeleditor.py
+++ modeleditor.py
@@ -26,3 +26,23 @@ class ROMEModelEditor(ModelEditor):
         sys.path.remove('.')
         os.chdir('..')
         return new_model
+
+
+class MENDModelEditor(ModelEditor):
+
+    def edit_model(self, model, tokenizer, fact):
+        # TODO: Fixup imports
+        os.chdir('./rome')
+        sys.path.append('.')
+        from baselines.mend import MENDHyperParams, MendRewriteExecutor
+
+        subject = fact.get_subject_label()
+        target = fact.get_target_label()
+        prompt = fact.get_fact_prompt().replace(subject, '{}')
+        requests = [{'prompt': prompt, 'subject': subject, 'target_new': {'str': target}}]
+        hparams = MENDHyperParams.from_json('hparams/MEND/gpt2-xl.json')
+        new_model, _ = MendRewriteExecutor().apply_to_model(model, tokenizer, requests, hparams)
+
+        sys.path.remove('.')
+        os.chdir('..')
+        return new_model
--- testrunner.py
+++ testrunner.py
@@ -45,7 +45,7 @@ class TestRunner:
 
 if __name__ == '__main__':
     from queryexecutor import GPT2QueryExecutor
-    from modeleditor import ROMEModelEditor
+    from modeleditor import ROMEModelEditor, MENDModelEditor
     from fact import Fact
     from relation import Relation
     from query import Query
@@ -55,6 +55,6 @@ if __name__ == '__main__':
     tq = Query('Q12379', Relation.BROTHER, ['Q76'])  # Mario's brother is Barack Obama
     cq = Query('Q12379', Relation.BROTHER, ['Q210593'])  # Mario's brother is Luigi
     tc = TestCase(tq, [])
-    tr = TestRunner(GPT2QueryExecutor(), ROMEModelEditor())
+    tr = TestRunner(GPT2QueryExecutor(), MENDModelEditor())
     res = tr.run_testcases(f, [tc])
     print(res)
