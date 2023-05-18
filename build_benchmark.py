469832b82e940d58c7ffca342e77033fea0b789b
roicohen93 <roi3993@gmail.com>
2023-05-19 01:24:09 +0200

    updates

--- build_benchmark.py
+++ build_benchmark.py
@@ -215,7 +215,7 @@ if __name__ == '__main__':
     # for example in dataset.sample(5):
     #     print(example)
 
-    top_views_benchmark = construct_fake_dataset_based_on_top_views_file(limit=2000, limit_num_of_facts=3, limit_subjects=20000)
+    top_views_benchmark = construct_fake_dataset_based_on_top_views_file(limit=1000, limit_num_of_facts=3, limit_subjects=20000)
     top_views_benchmark.to_file('./benchmark/top_views_1000.json')
 
 
