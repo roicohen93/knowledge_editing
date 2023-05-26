832bed43949ddac498e711f0d54b2cfb77ab14b7
Eden Biran <edenbiran@mail.tau.ac.il>
2023-05-26 14:09:57 +0300

    Change Llama configs

--- queryexecutor.py
+++ queryexecutor.py
@@ -111,11 +111,10 @@ class LlamaQueryExecutor(HFQueryExecutor):
         self._model_size = model_size
         self._model_name = f'llama-{self._model_size}'
         if tokenizer is None:
-            tokenizer = LlamaTokenizer.from_pretrained(f'decapoda-research/{self._model_name}-hf')
-            tokenizer.eos_token = '<|endoftext|>'
+            tokenizer = AutoTokenizer.from_pretrained(f"huggyllama/{self._model_name}")
             tokenizer.pad_token = tokenizer.eos_token
         if model is None:
-            model = LlamaForCausalLM.from_pretrained(f'decapoda-research/{self._model_name}-hf', device_map="auto", offload_folder="offload", torch_dtype=torch.float16, offload_state_dict=True, pad_token_id=tokenizer.eos_token_id)
+            model = LlamaForCausalLM.from_pretrained(f"huggyllama/{self._model_name}", device_map="auto", offload_folder="offload", torch_dtype=torch.float16, offload_state_dict=True)
         super().__init__(model, tokenizer, device)
 
     def get_model_name(self):
